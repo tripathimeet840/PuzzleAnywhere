c666118aa07a3f4b747f6cf121927c5e5c9444e1
tripathimeet840 <tripathimeet840@gmail.com>
2025-02-28 22:56:26 +0530

    New Scenes Added
    Loading Scene
    Home Scene

---
+++ Assets/Game/Script/LoadingScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour
+{
+
+    [SerializeField]
+    Slider slider;
+    float value;
+    bool loadingComplete;
+
+    void Start()
+    {
+        if(slider.value == slider.minValue && loadingComplete == false)
+        {
+           // IncreaseSliderValue(slider.value);
+            InvokeRepeating("IncreaseSliderValue", 0,1);
+        }
+            
+
+    }
+
+    void Update()
+    {
+        
+
+
+    }
+
+    private void IncreaseSliderValue()
+    {
+
+        value = slider.value;
+        value = value + 0.1f;
+       // yield return new WaitForSeconds(1);
+        slider.value = value;
+
+
+        if (loadingComplete == false && slider.value == slider.maxValue)
+        {
+            Debug.Log(" if");
+
+            loadingComplete = true;
+            SceneManager.LoadScene(1);
+            
+        }
+        else
+        {
+
+            
+            Debug.Log(" else");
+        }
+    }
+
+}
